f776f69352a15e26f77756d0504f16cb8ff92e97
Essel Luna <essel.luna1@trans-cosmos.co.jp>
2023-10-12 22:08:19 +0800

    update indent and deleted unnecessary css

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ const openButton = document.querySelector('.openBtn');
 const closeButton = document.querySelector('.closeBtn');
 
 openButton.addEventListener('click', function () {
-    menu.parentElement.classList.toggle('show');
-    menu.style.display = "block";
-    closeButton.style.display = "block";
-    openButton.style.display = "none";
+  menu.parentElement.classList.toggle('show');
+  menu.style.display = "block";
+  closeButton.style.display = "block";
+  openButton.style.display = "none";
 })
 
 closeButton.addEventListener('click', function () {
-    closeButton.style.display = "none";
-    openButton.style.display = "block";
-    menu.style.display = "none";
-    menu.parentElement.classList.toggle('show');
+  closeButton.style.display = "none";
+  openButton.style.display = "block";
+  menu.style.display = "none";
+  menu.parentElement.classList.toggle('show');
 })
